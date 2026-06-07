db258138adc49b308062a60595917c3ed70cef1e
endalk200 <eb808826@gmail.com>
2026-06-07 17:12:36 +0300

    chore: remove previous review command implementation and tool implementations

--- packages/agents/code-review/src/index.ts
+++ packages/agents/code-review/src/index.ts
@@ -1,16 +1,1 @@
-export {
-	type CodeReviewAgentError,
-	CodeReviewAgentRuntimeError,
-	formatCodeReviewAgentError,
-	ReviewTargetCollectionError,
-} from "./errors.js";
-export {
-	DevToolsMiddlewareLoader,
-	type DevToolsMiddlewareLoaderShape,
-	makeReviewTools,
-	ReviewModelExecutor,
-	type ReviewModelExecutorShape,
-	type ReviewModelRequest,
-	ToolRuntimeLayer,
-} from "./executor.js";
-export { CodeReviewAgent, CodeReviewAgentLayer } from "./service.js";
+export {};
